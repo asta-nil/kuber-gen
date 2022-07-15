0b5db32a889bd8f2f477232b6a1ca87e53fe3b3a
astanil <astakhovnil@gmail.com>
2022-07-16 02:08:03 +0300

    Edited flake8 error

--- app/api.py
+++ app/api.py
@@ -14,9 +14,7 @@ db_pass = os.environ.get('MYSQL_PASSWORD')
 db_hostname = os.environ.get('MYSQL_HOST')
 db_name = os.environ.get('MYSQL_DATABASE')
 
-DB_URI = 'mysql+pymysql://{db_username}:{db_password}@{db_host}/{database}'.format(
-    db_username=db_user, db_password=db_pass, 
-    db_host=db_hostname, database=db_name)
+DB_URI = 'mysql+pymysql://{db_username}:{db_password}@{db_host}/{database}'.format(db_username=db_user, db_password=db_pass, db_host=db_hostname, database=db_name)
 
 engine = create_engine(DB_URI, echo=True)
 
